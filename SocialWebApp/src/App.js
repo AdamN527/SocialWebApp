3b131b5b94705fdea403327c4e464bd565d5099b
Adam Najib <anajib527@gmail.com>
2022-01-13 19:14:45 -0800

    Initial

---
+++ SocialWebApp/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Sidebar from './Sidebar';
+
+function App() {
+  return (
+    <div className="app">
+      <h1> Hey</h1>
+      {/*Sidebar*/}
+      <Sidebar />
+      {/*Feed*/}
+      {/*Widgets*/}
+    </div>
+  );
+}
+
+export default App;
